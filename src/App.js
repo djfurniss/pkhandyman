7ff0eedb2960272b27abea49a474c63a1e7654a7
Denasia <dfurniss@ymail.com>
2023-09-05 16:47:38 -0500

    imported styles above imported Header component; added id to div

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Header from "./Header";
 import './styles/App.css';
+import Header from "./Header";
 
 export default function App() {
   return (
-    <div>
+    <div id="App">
       <Header/>
     </div>
   );
