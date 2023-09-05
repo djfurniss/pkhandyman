55bbc85d977ab7447b7c3a62c0888c106029fcfb
Denasia <dfurniss@ymail.com>
2023-09-05 16:49:09 -0500

    header outlined and styled

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,16 @@ import logo from './utils/p&k-logo.jpeg';
 
 export default function Header(){
     return(
-        <div id="Header">
-            {/* <h2>header</h2> */}
+        <div id="Header" className="row">
             <img src={logo} alt="logo"/>
+            <div id="text-container" className="row">
+                    <h1>P&K Handyman Services</h1>
+                    <div className="row">
+                        <p>Home</p>
+                        <p>Services</p>
+                        <p>Get a Quote</p>
+                    </div>
+            </div>
         </div>
     )
 };
